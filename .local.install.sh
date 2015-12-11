05b3f2d6e6eb0f0d511b58b2f8799e0f47715ae8
1dose <odobrescu@gmail.com>
2015-12-11 16:21:09 -0500

    Update .local.install.sh

--- .local.install.sh
+++ .local.install.sh
@@ -1,17 +1,43 @@
 #!/bin/sh
 apt-get update  # To get the latest package lists
 
-# install flux, turns screen to reddish at night (eases strain on eyes)
+# install redshift (alternative to flux), turns screen to reddish at night (eases strain on eyes)
+gtk-redshift -l 43:-79 #setup for toronto, for new lat:long http://itouchmap.com/latlong.html
+
+
 add-apt-repository ppa:kilian/f.lux
 apt-get update  # Flux requires update
 apt-get install fluxgui
 
+# install notepad++, text editor for notes (good autosave on crash feature)
+add-apt-repository ppa:notepadqq-team/notepadqq
+apt-get update
+apt-get install notepadqq
+
+# install guake, desktop environment for moving windows around the screen and acessing terminal quickly
+apt-get install guake
+
+# fix schema error found on ubuntu 14.4 - guake
+mkdir /etc/gconf/schemas
+cd /etc/gconf/schemas/
+ln -s /usr/share/gconf/schemas/guake.schemas
+# fix warning found on ubuntu 14.4 - guake
+apt-get install --reinstall gtk2-engines
+
+
+
+
+
+cp /usr/share/applications/guake.desktop /etc/xdg/autostart/ # allow guake to auto-start on login
 
 # reminders for actions to take after install script is done
+echo setup launcher-panel: notepad++, chrome, cmd, gitter,
+echo click redshift and add autostart functionality
 echo google chrome, login for extensions
 echo install lastpass
 echo setup flux from fluxgui, run fluxgui
 echo get webstorm, install vim plugin
+
 
 - !!python/unicode 'cscope'
 - !!python/unicode 'ctags'
